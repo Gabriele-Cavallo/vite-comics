54363f55ecc41b2e9975dc714986c097cbda6267
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-04-11 14:54:23 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+    // import name-app from "./components/name-app.vue";
+    export default{
+        // components: {
+        //     name-app,
+        // }
+    }
+</script>
+
+<template>
+    <h1>Ciao Sono un titolo</h1>
+</template>
+
+<style lang="scss">
+    @use './style/generic';
+</style>
